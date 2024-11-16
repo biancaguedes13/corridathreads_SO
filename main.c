2bb19aed04be83a0d5348a3ab2fcdd34c754e4fe
biancaguedes13 <148780930+biancaguedes13@users.noreply.github.com>
2024-11-16 19:53:49 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,24 +3,32 @@
 #include <time.h>
 #include "threads.h"
 
-#define NUM_COELHOS 2
+#define NUM_LEBRES 2
 #define NUM_TARTARUGAS 2
 
 int main() {
     srand(time(NULL));
 
-    int total_competidores = NUM_COELHOS + NUM_TARTARUGAS;
+    int total_competidores = NUM_LEBRES + NUM_TARTARUGAS;
     Competidor competidores[total_competidores];
     pthread_t threads[total_competidores];
 
-    // Inicializa coelhos
-    for (int i = 0; i < NUM_COELHOS; i++) {
-        competidores[i] = (Competidor){"Coelho", i + 1, 0, 3, 50}; // 50% chance de descansar
+    // Inicializa lebres
+    for (int i = 0; i < NUM_LEBRES; i++) {
+        competidores[i].tipo = "Lebre";
+        competidores[i].id = i + 1;
+        competidores[i].posicao = 0;
+        competidores[i].velocidade = 3;
+        competidores[i].chance_descanso = 50; 
     }
 
     // Inicializa tartarugas
     for (int i = 0; i < NUM_TARTARUGAS; i++) {
-        competidores[NUM_COELHOS + i] = (Competidor){"Tartaruga", i + 1, 0, 1, 0}; // Nunca descansa
+        competidores[NUM_LEBRES + i].tipo = "Tartaruga";
+        competidores[NUM_LEBRES + i].id = i + 1;
+        competidores[NUM_LEBRES + i].posicao = 0;
+        competidores[NUM_LEBRES + i].velocidade = 1;
+        competidores[NUM_LEBRES + i].chance_descanso = 0; 
     }
 
     // Cria threads
