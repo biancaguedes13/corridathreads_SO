49134667abf9fdfe162167ba286e580182dffe30
biancaguedes13 <148780930+biancaguedes13@users.noreply.github.com>
2024-11-16 19:27:32 -0300

    Update threads.h

--- threads.h
+++ threads.h
@@ -1,1 +1,17 @@
+#ifndef THREADS_H
+#define THREADS_H
+
+#define DISTANCIA_TOTAL 100
+
+typedef struct {
+    char *tipo;
+    int id;
+    int distancia;
+    int velocidade;
+    int chance_descanso;
+} Competidor;
+
+void *correr(void *arg);
+
+#endif
 
